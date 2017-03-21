3948beb2c5cfe57a3aede727edc9ec837b95e5f6
Yeison <yeisontorrado@termotecnica.com.co>
2017-03-21 17:44:57 -0500

    footer

--- application/views/principal/footer.php
+++ application/views/principal/footer.php
@@ -1,5 +1,5 @@
 		<footer>
             <p>Todos los derechos reservados a Termotecnica Coindustrial S.A. Ingenieros Contratista.</p>
             <p>Dpto. de Sistemas Cúcuta</p>
-            <p>Año 2015</p>
+            <p>Año <?= date("Y")?></p>
 		</footer>
